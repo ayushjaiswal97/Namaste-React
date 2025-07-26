6122c10b5cd09acd8e28acfb6231bfcc270d6045
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-26 12:09:01 +0530

    Refactor App.js to include a React element and improve component composition

--- Ep03_LayingTheFoundation/App.js
+++ Ep03_LayingTheFoundation/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>React Element inside React Element</span>;
 
 const Title = () => (
   <h1 className="head" tabIndex="5">
+    {/* {elem} */}
     Namaste React using JSX🚀
   </h1>
 );
 
-// Component composition
 const HeadingComponent = () => (
-    <div id="container">
+    <div id="container">  
+        {Title()}
         <Title />
+        <Title></Title>
       <h1 className="head">Namaste React using Functional Component🚀</h1>
     </div>
 );
+
+// Component composition
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
