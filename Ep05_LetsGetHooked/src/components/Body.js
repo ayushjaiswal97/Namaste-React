be55fa82aad10b42d1ffaa691d1b5f89e93c4ac3
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-28 20:14:58 +0530

    Local State Variable(hooks)

--- Ep05_LetsGetHooked/src/components/Body.js
+++ Ep05_LetsGetHooked/src/components/Body.js
@@ -1,22 +1,64 @@
 import React from 'react';
 import RestaurantCard from './RestaurantCard';
 import resList from '../utils/mockData';
+import { useState } from 'react';
 
 const Body = () => {
-
+    // Local State Variable - Super Powefull Variable 
+    const [listOfRestaurants, setListOfRestaurants] = useState([
+        {
+        data: {
+            id: "1",
+            name: "Restaurant A",
+            imageId: "bdcd233971b7c81bf77e1fa4471280eb",
+            cuisines: ["Indian", "Chinese"],
+            costForTwo: 50000,
+            deliveryTime: 30,
+            avgRating: 3.8,
+        }
+    },
+     {
+        data: {
+            id: "2",
+            name: "Restaurant B",
+            cuisines: ["Italian", "Mexican"],
+            costForTwo: 60000,
+            deliveryTime: 25,
+            imageId: "bdcd233971b7c81bf77e1fa4471280eb",
+            avgRating: 4.2,
+        }
+    },
+     {
+        data: {
+            id: "3",
+            name: "Restaurant C",
+            cuisines: ["American", "French"],
+            costForTwo: 70000,
+            deliveryTime: 20,
+            imageId: "bdcd233971b7c81bf77e1fa4471280eb",
+            avgRating: 4.8,
+        }
+    }
+]);
     
     return (
         <div className="body">
             <div className="filter">
-                <button className="filter-btn" onClick={() => 
-                {console.log("Filter Clicked")
+                <button className="filter-btn"
+                onClick={() => {
+                    // Filter logic here
+                    const filteredList = listOfRestaurants.filter(
+                        (res) => res.data.avgRating > 4.0
+                    );
+                    setListOfRestaurants(filteredList);
                 }}>
                 Top Rated Restaurants
                 </button>
             </div>
+
             <div className="res-container">
                 {
-                resList.map((restaurant) => (
+                listOfRestaurants.map((restaurant) => (
                 <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
                 ))}
             </div>
