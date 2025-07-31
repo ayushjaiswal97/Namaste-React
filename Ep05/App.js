81919adb601229440bf427c3b76a042e2a927178
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-31 18:01:56 +0530

    MultiLine JSX

--- Ep05/App.js
+++ Ep05/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Using - JSX
-// How to add class in jsx -className
-const jsxHeading = <h1 className="heading">Namaste React using JSX</h1>;
-console.log(jsxHeading);
+// MultiLine JSX - wrap with ()
+  const jsxHeading = (<h1
+   className="heading">Namaste React using JSX
+  </h1>);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+  console.log(jsxHeading);
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(jsxHeading);
--- Ep05/readme.md
+++ Ep05/readme.md
@@ -23,4 +23,6 @@
 
 - JSX (transpiled before it reaches the JS Engine) - Pracel (transpiled by Pracel) - Babel - (is transpiled  quickly transpiled our code it takes conert your jsx code into react code).
 - JSX (HTML or XML like syntax) => React.createElement => Object => HTML (Browser Understands)
-- JSX => Babel Transpiles it to React.createElement => Object => HTML (render)-(Browser Understands)
+- JSX => Babel Transpiles it to React.createElement => ReactElement-JS Object => HTML (render)-(Browser Understands)
+
+- MultiLine JSX - wrap with ()
