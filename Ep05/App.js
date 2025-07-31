ea3fc552dd530d55ace38e3855a7042ba1f8c114
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-31 17:48:59 +0530

    use className instead of id

--- Ep05/App.js
+++ Ep05/App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 // Using - JSX
 // How to add class in jsx -className
-const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+const jsxHeading = <h1 className="heading">Namaste React using JSX</h1>;
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
