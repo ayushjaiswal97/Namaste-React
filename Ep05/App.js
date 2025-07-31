62550b26f7a18140e37381a037b3421198c2231c
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-31 18:36:26 +0530

    component compostion

--- Ep05/App.js
+++ Ep05/App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Using - JSX
-// React Element
-  const Heading = (
-  <h1
-   className="heading">Namaste React using JSX
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
   </h1>
-  );
+);
 
-  // React Component
- // React Functional Component - Just a Normal JS function.
- const HeadingComponent = () => {
-  return (
-    <h1>This is a React Functional Component</h1>
-  )
- };
-
- const HeadingComponent2 = () => {
-  return <h2>React Functional Component2</h2>
- };
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<HeadingComponent2/>);
+  root.render(<HeadingComponent/>);
