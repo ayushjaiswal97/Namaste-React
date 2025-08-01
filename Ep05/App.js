3b59209b2bb7a810a8602cf5f1f497be123bac0c
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-08-01 16:00:07 +0530

    using  destructuring for dynamically pass data

--- Ep05/App.js
+++ Ep05/App.js
@@ -35,14 +35,14 @@ const Header = () => {
   )
 }
 
-const RestaurantCard = (props) => {
+const RestaurantCard = ({resName, cuisine}) => {
   return(
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
      <img className="res-logo"
       src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/2/17/5697054d-380a-4c96-8f07-a10d07c5e3f8_613953.jpg" 
       alt="res-logo" />
-     <h3>{props.resName}</h3>
-     <h4>{props.cuisine}</h4>
+     <h3>{resName}</h3>
+     <h4>{cuisine}</h4>
      <h4>4.1 stars</h4>
      <h4>38 minutes</h4>
     </div>
