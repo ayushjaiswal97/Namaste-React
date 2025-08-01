584a4bed8cf51c72f142a9be15bc52a56a43012f
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-08-01 15:55:53 +0530

    Make a card to be dynamically (props)

--- Ep05/App.js
+++ Ep05/App.js
@@ -35,14 +35,14 @@ const Header = () => {
   )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return(
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
      <img className="res-logo"
       src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/2/17/5697054d-380a-4c96-8f07-a10d07c5e3f8_613953.jpg" 
       alt="res-logo" />
-     <h3>Sharma & Vishnu Fast Food, Hamidiya Road</h3>
-     <h4>Indian, Snacks, Mughlai, Beverages, Fast Food</h4>
+     <h3>{props.resName}</h3>
+     <h4>{props.cuisine}</h4>
      <h4>4.1 stars</h4>
      <h4>38 minutes</h4>
     </div>
@@ -54,7 +54,8 @@ const Body = () =>{
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-         <RestaurantCard/>
+         <RestaurantCard resName="Sharma & Vishnu Fast Food" cuisine="Indian, Snacks, Mughlai, Beverages, Fast Food"/>
+         <RestaurantCard resName="KFC" cuisine="Burger"/>
       </div>
     </div>
   )
