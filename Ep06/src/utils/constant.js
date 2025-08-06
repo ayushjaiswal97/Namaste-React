91ff876b0a99185d248664519c3c3f22fcebbead
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-08-06 11:38:06 +0530

    Episode 6 work complete

--- Ep06/src/utils/constant.js
+++ Ep06/src/utils/constant.js
@@ -1,3 +1,5 @@
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lecture-3624ld.png";
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const Food_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.062017821881533&lng=72.54186391723825&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
