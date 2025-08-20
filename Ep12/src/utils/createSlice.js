dc3774e57755ea90f0b862dcaf920f5c300e6a5c
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-08-20 18:22:48 +0530

    using return new object [ ] will be replaced inside originalState

--- Ep12/src/utils/createSlice.js
+++ Ep12/src/utils/createSlice.js
@@ -6,22 +6,20 @@ const cartSlice = createSlice({
         items: [],
     },
     reducers:{
-        addItems: (state, action) =>{
-
-            // vanialla(older) Redux => DON'T MUTATE STATE
-            // const newState = [...state];
-            // newState.items.push(action.payload);
-            // return newState;
-
-            // Redux Toolkit 
+        addItems: (state, action) => {
+            // Redux Toolkit -> uses immer behind the scene
             // Mutating the state here
             state.items.push(action.payload);
         },
         removeItems: (state, action) =>{
             state.items.pop();
-        },
+        }, 
+
         clearItems: (state, action) =>{
-            state.items.length = 0; //[]
+            // RTK - either Mutate the existing state or return a new State
+            // state.items.length = 0; //originalState = []
+
+            return { items: [] }; // this new object [] will be replaced inside originalState = { items: [] }
         },
     },
 });
